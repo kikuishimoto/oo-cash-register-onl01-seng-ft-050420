6fe2298ae63bd4adcbc9bbe4d7c71cbe9b4f26f9
kiku ishimoto <kikuish@gmail.com>
2020-05-15 00:18:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+
+
+  end
+
+  def add_item(item, price, quanity = 1)
+  if quanity > 1
+    i = 0
+    while i < quanity
+      @items << item
+      i += 1
+    end
+  else
+    @items << item
+  end
+
+  @total = price * quanity
+
+  @total
+  end
+
+  def apply_discount
+    if @discount != 0
+      @discount = (@discount.to_f / 100)
+      @discount = @total.to_f * @discount
+      @total = @total - @discount
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+    return "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+end
